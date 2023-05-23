9891abaf513d518e5b7e7769647209ff98e4981e
tibor molnar <tmolnair@gmail.com>
2023-05-23 21:43:25 +0100

    deploy app to render

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,34 @@
 //import adapter from '@sveltejs/adapter-auto';
-import adapter from "@sveltejs/adapter-netlify";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+//import adapter from "@sveltejs/adapter-netlify";
+//import { vitePreprocess } from "@sveltejs/kit/vite";
 
-/** @type {import('@sveltejs/kit').Config} */
+//** @type {import('@sveltejs/kit').Config} */
+/*
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
+   Consult https://kit.svelte.dev/docs/integrations#preprocessors
+   for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
     adapter: adapter()
+  }
+};
+
+export default config;
+*/
+
+/** @type {import('@sveltejs/kit').Config} */
+
+import node from '@sveltejs/adapter-node';
+
+const config = {
+  kit: {
+    adapter: node(),
+
+    // Override http methods in the Todo forms
+    methodOverride: {
+      allowed: ['PATCH', 'DELETE']
+    }
   }
 };
 
